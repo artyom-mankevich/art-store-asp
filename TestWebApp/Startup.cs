693100f99ce0cfbe7eaccbcdbe259f3c137035df
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-08-01 14:17:50 +0300

    Added authorization and authetication via cookies or token, added configuration file prop

--- TestWebApp/Startup.cs
+++ TestWebApp/Startup.cs
@@ -1,34 +1,64 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using MyWebApp.Data;
+using MyWebApp.Data.Entities;
 using MyWebApp.Services;
+using Newtonsoft.Json;
 
 namespace MyWebApp
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DutchContext>(cfg =>
+            services.AddIdentity<StoreUser, IdentityRole>(cfg =>
             {
-                cfg.UseSqlServer();
-            });
+                cfg.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<DutchContext>();
             
+            services.AddAuthentication()
+                .AddCookie()
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = _configuration["Tokens:Issuer"],
+                        ValidAudience = _configuration["Tokens:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]))
+                    };
+                });
+            
+            services.AddDbContext<DutchContext>(cfg => { cfg.UseSqlServer(); });
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddTransient<DutchSeeder>();
             services.AddTransient<IMailService, NullMailService>();
             services.AddScoped<IDutchRepository, DutchRepository>();
-            
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation()
+                .AddNewtonsoftJson(cfg =>
+                    cfg.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
             services.AddRazorPages();
         }
 
@@ -46,6 +76,9 @@ namespace MyWebApp
 
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
             app.UseEndpoints(cfg =>
             {
                 cfg.MapRazorPages();
