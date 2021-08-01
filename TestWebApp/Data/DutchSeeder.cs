2569c5bbb833d3af5f4298fe48bc41fecb99ec40
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-08-01 14:12:06 +0300

    Added user creation upon DB seeding in DutchSeeder.cs

--- TestWebApp/Data/DutchSeeder.cs
+++ TestWebApp/Data/DutchSeeder.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using MyWebApp.Data.Entities;
 using Newtonsoft.Json;
 using JsonSerializer = System.Text.Json.JsonSerializer;
@@ -13,17 +15,37 @@ namespace MyWebApp.Data
     {
         private readonly DutchContext _context;
         private readonly IWebHostEnvironment _environment;
+        private readonly UserManager<StoreUser> _userManager;
 
-        public DutchSeeder(DutchContext context, IWebHostEnvironment environment)
+        public DutchSeeder(DutchContext context, IWebHostEnvironment environment, UserManager<StoreUser> userManager)
         {
             _context = context;
             _environment = environment;
+            _userManager = userManager;
         }
 
-        public void Seed()
+        public async Task SeedAsync()
         {
             _context.Database.EnsureCreated();
 
+            StoreUser user = await _userManager.FindByEmailAsync("artyom@mail.com");
+            if (user == null)
+            {
+                user = new StoreUser()
+                {
+                    FirstName = "Artyom",
+                    LastName = "Mankevich",
+                    Email = "artyom@mail.com",
+                    UserName = "artyom"
+                };
+
+                var result = await _userManager.CreateAsync(user, "Password1!");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create new user in seeder");
+                }
+            }
+            
             if (!_context.Products.Any())
             {
                 var filepath = Path.Combine(_environment.ContentRootPath, "Data/art.json");
@@ -33,6 +55,7 @@ namespace MyWebApp.Data
                 _context.Products.AddRange(products);
                 var order = new Order()
                 {
+                    User = user,
                     OrderDate = DateTime.Today,
                     OrderNumber = "10000",
                     Items = new List<OrderItem>()
