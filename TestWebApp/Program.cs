c222fd1ca5272df059e12aab15ee62d337902b5b
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-08-01 14:16:02 +0300

    Replaced Seed operation with async one

--- TestWebApp/Program.cs
+++ TestWebApp/Program.cs
@@ -28,7 +28,7 @@ namespace MyWebApp
             using (var scope = scopeFactory.CreateScope())
             {
                 var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
-                seeder.Seed();
+                seeder.SeedAsync().Wait();
             }
         }
 
